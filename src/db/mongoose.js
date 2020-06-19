cd445c88168fad8f67c25e19ea37da9048ffb121
Philip Victor Jebaraj S <philip.jebaraj@hotmail.com>
2020-06-19 22:30:21 +0530

    MONGODB_URL changed

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
