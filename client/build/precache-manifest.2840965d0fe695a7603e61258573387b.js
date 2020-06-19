c9b46bb5c4fc0c4e815271f9be666570f35768ea
Philip Victor Jebaraj S <philip.jebaraj@hotmail.com>
2020-06-19 21:23:03 +0530

    Generated build

---
+++ client/build/precache-manifest.2840965d0fe695a7603e61258573387b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43cca28d2086179d78cbca821b26d7f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73ac810915143a094cd3",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "edaad4a1d0997c88fa15",
+    "url": "/static/css/main.c11da882.chunk.css"
+  },
+  {
+    "revision": "73ac810915143a094cd3",
+    "url": "/static/js/2.d8de24e3.chunk.js"
+  },
+  {
+    "revision": "c3f4a91b08326551c3d2c23a3296dfa8",
+    "url": "/static/js/2.d8de24e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edaad4a1d0997c88fa15",
+    "url": "/static/js/main.bebc2bdf.chunk.js"
+  },
+  {
+    "revision": "1226bb972a7ee37cf787",
+    "url": "/static/js/runtime-main.57d45069.js"
+  }
+]);
