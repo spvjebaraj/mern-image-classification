06c14b473c2ea77201a8f4f4bd4c70bcc0e6d5a5
Philip Victor Jebaraj S <philip.jebaraj@hotmail.com>
2020-06-19 23:08:49 +0530

    folder name changed to server

--- server/app.js
+++ server/app.js
@@ -14,10 +14,10 @@ app.use(userRouter);
 app.use(classificationRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static(path.join(__dirname, "/../client", "build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "/../client", "build", "index.html"));
   });
 }
 
