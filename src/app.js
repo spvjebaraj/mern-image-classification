aec9e7c255b75e7a66b4a3569923d0ee556c4283
Philip Victor Jebaraj S <philip.jebaraj@hotmail.com>
2020-06-19 21:46:12 +0530

    Updated heroku details

--- src/app.js
+++ src/app.js
@@ -12,10 +12,13 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(userRouter);
 app.use(classificationRouter);
-app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 module.exports = app;
